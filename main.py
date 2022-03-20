2bef0aa2c6cb53022dbcb622f600645bf8b58135
Josué Amaya <josue@ese.plus>
2022-03-19 18:18:31 -0600

    endpoint to download audio from youtube and facebook videos

--- main.py
+++ main.py
@@ -6,32 +6,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-class UrlType(str, Enum):
-    yt = "yt"
-    vimeo = "vimeo"
-
-
-@app.get("/urls/")
-def get_dash_url(url: str):
-    yt_result = os.popen(
-        "youtube-dl -f \"bestvideo[height<=480]+bestaudio/best[height<=480]\" --merge-output-format mp4  -g {0}".format(
-            url))
-    new_url = yt_result.read().strip()
-
-    return {"url": new_url}
-
-
-@app.get("/audio")
+@app.get("/download")
 def download_audio(url: str):
-    yt_result = os.popen("youtube-dl -f 'bestaudio[ext=m4a]' -g {0}".format(url))
-    new_url = yt_result.read().strip()
-
-    return {"url": new_url}
-
-
-@app.get("/formats")
-def get_formats(url: str):
-    yt_result = os.popen("youtube-dl --dump-json {0}".format(url))
+    yt_result = os.popen("youtube-dl -j -f 'bestaudio[ext=m4a]' {0}".format(url))
     data = yt_result.read().strip()
 
-    return json.loads(data)
+    video_info = json.loads(data)
+
+    return video_info
