fa677f8f2701cfab48e9b392d7eae9d48d80bc63
Josué Amaya <josue@ese.plus>
2022-03-19 18:58:01 -0600

    endpoint to download audio from youtube and facebook videos

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 import os
-from enum import Enum
+from urllib import request
 from fastapi import FastAPI
+from starlette.responses import FileResponse, StreamingResponse
 
 app = FastAPI()
 
 
-@app.get("/download")
-def download_audio(url: str):
+@app.get("/audio")
+def audio_url(url: str):
     yt_result = os.popen("youtube-dl -j -f 'bestaudio[ext=m4a]' {0}".format(url))
     data = yt_result.read().strip()
 
